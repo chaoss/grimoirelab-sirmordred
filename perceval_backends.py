32bff81abd525b6b237f8f917e6699105de6ec75
Luis Cañas Díaz <lcanas@bitergia.com>
2016-08-25 18:41:53 +0200

    Add basic creation for RQ workers based on configuration files
    Creation of RQ workers is added based on the number of configuration
    files. Also checks if RQ workers are already created

---
+++ perceval_backends.py
@@ -0,0 +1,3 @@
+PERCEVAL_BACKENDS=['bugzilla','bugzillarest','confluence','discourse',
+    'gerrit','git','github','gmane','jenkins','jira','kitsune','mbox',
+    'mediawiki','pipermail','remo','stackexchange','supybot','telegram']
