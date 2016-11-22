a4f65d533fb6d5e6fefb254f2a3cf4299c3f28cb
quan <zhquan7@gmail.com>
2016-11-22 11:14:53 +0100

    Convert projects.json to hierarchy.yml and projects-repos.yml

---
+++ utils/projects_json2yml.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#   Quan Zhou <quan@bitergia.com>
+#
+
+
+import argparse
+import json
+import yaml
+
+
+def write_yaml(file_name, data):
+    with open(file_name, 'w+') as f:
+        yaml.dump(data, f, default_flow_style=False)
+
+def open_file(file_name):
+    data = open(file_name).read()
+    json_data = json.loads(data)
+
+    return json_data
+
+def read_arguments():
+    desc="Convert JSON to YML: return hierarchy.yml and projects_repo.yml"
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description=desc)
+
+    parser.add_argument("json_file",
+                        action="store",
+                        help="JSON file: input")
+
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == "__main__":
+    args = read_arguments()
+    json_file = args.json_file
+    json_data = open_file(json_file)
+
+    hierarchy_list = {}
+    repos = {}
+    not_backend = ["title", "description", "dev_list", "gerrit_repo"]
+    for data in json_data["projects"]:
+        #hierarchy
+        if len(json_data["projects"][data]['parent_project']) == 0:
+            hierarchy_list[data] = []
+        else:
+            for key in json_data["projects"][data]['parent_project']:
+                hierarchy_list[data][key] = []
+
+        #projects_repo
+        repos[data] = {"meta": {"title": json_data["projects"][data]["title"].lower()}}
+        for backend_name in json_data["projects"][data]:
+            backend = json_data["projects"][data][backend_name]
+            if len(backend) > 0 and backend_name not in not_backend or backend_name == "gerrit_repo" and len(backend[0]) > 0:
+                repo_list = []
+                for repo in backend:
+                    repo_list.append(repo['url'])
+                repos[data][backend_name] = repo_list
+
+    write_yaml("hierarchy.yml", hierarchy_list)
+    write_yaml("projects-repos.yml", repos)
