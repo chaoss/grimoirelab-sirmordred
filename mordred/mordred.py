de4b8ac4016ffa4472294dc61b28c13f0ecdd26c
Alvaro del Castillo <acs@bitergia.com>
2017-03-13 06:09:22 +0100

    [config] Move config management to its own class

--- mordred/mordred.py
+++ mordred/mordred.py
@@ -22,20 +22,16 @@
 #     Alvaro del Castillo <acs@bitergia.com>
 #
 
-import configparser
-import json
 import logging
 import queue
 import requests
-import sys
 import threading
 import time
 import traceback
 
 from datetime import datetime, timedelta
 
-from grimoire_elk.utils import get_connectors
-
+from mordred.config import Config
 from mordred.error import ElasticSearchError
 from mordred.error import DataCollectionError
 from mordred.error import DataEnrichmentError
@@ -46,9 +42,6 @@ from mordred.task_identities import TaskIdentitiesCollection, TaskIdentitiesInit
 from mordred.task_manager import TasksManager
 from mordred.task_panels import TaskPanels, TaskPanelsMenu
 
-
-SLEEPFOR_ERROR = """Error: You may be Arthur, King of the Britons. But you still """ + \
-"""need the 'sleep_for' variable in sortinghat section\n - Mordred said."""
 
 ES_ERROR = "Before starting to seek the Holy Grail, make sure your ElasticSearch " + \
 "at '%(uri)s' is available!!\n - Mordred said."
@@ -61,92 +54,7 @@ logger = logging.getLogger(__name__)
 class Mordred:
 
     def __init__(self, conf_file):
-        self.conf_file = conf_file
-        self.conf = None
-        self.conf = self.__read_conf_files()
-
-    def __read_conf_files(self):
-        conf = {}
-
-        logger.debug("Reading conf files")
-        config = configparser.ConfigParser()
-        config.read(self.conf_file)
-        logger.debug(config.sections())
-
-        if 'min_update_delay' in config['general'].keys():
-            conf['min_update_delay'] = config.getint('general','min_update_delay')
-        else:
-            # if no parameter is included, the update won't be performed more
-            # than once every minute
-            conf['min_update_delay'] = 60
-
-        # FIXME: Read all options in a generic way
-        conf['es_collection'] = config.get('es_collection', 'url')
-        conf['es_enrichment'] = config.get('es_enrichment', 'url')
-        conf['autorefresh_on'] = config.getboolean('es_enrichment', 'autorefresh')
-        conf['studies_on'] = config.getboolean('es_enrichment', 'studies')
-
-        projects_file = config.get('projects','projects_file')
-        conf['projects_file'] = projects_file
-        with open(projects_file,'r') as fd:
-            projects = json.load(fd)
-        conf['projects'] = projects
-
-        conf['collection_on'] = config.getboolean('phases','collection')
-        conf['identities_on'] = config.getboolean('phases','identities')
-        conf['enrichment_on'] = config.getboolean('phases','enrichment')
-        conf['panels_on'] = config.getboolean('phases','panels')
-
-        conf['update'] = config.getboolean('general','update')
-        try:
-            conf['kibana'] = config.get('general','kibana')
-        except configparser.NoOptionError:
-            pass
-
-        conf['sh_bots_names'] = config.get('sortinghat', 'bots_names').split(',')
-        # Optional config params
-        try:
-            conf['sh_no_bots_names'] = config.get('sortinghat', 'no_bots_names').split(',')
-        except configparser.NoOptionError:
-            pass
-        conf['sh_database'] = config.get('sortinghat', 'database')
-        conf['sh_host'] = config.get('sortinghat', 'host')
-        conf['sh_user'] = config.get('sortinghat', 'user')
-        conf['sh_password'] = config.get('sortinghat', 'password')
-        aux_matching = config.get('sortinghat', 'matching')
-        conf['sh_matching'] = aux_matching.replace(' ','').split(',')
-        aux_autoprofile = config.get('sortinghat', 'autoprofile')
-        conf['sh_autoprofile'] = aux_autoprofile.replace(' ','').split(',')
-        conf['sh_orgs_file'] = config.get('sortinghat', 'orgs_file')
-        conf['sh_load_orgs'] = config.getboolean('sortinghat', 'load_orgs')
-
-        try:
-            conf['sh_sleep_for'] = config.getint('sortinghat','sleep_for')
-        except configparser.NoOptionError:
-            if conf['identities_on'] and conf['update']:
-                logging.error(SLEEPFOR_ERROR)
-            sys.exit(1)
-
-        try:
-            conf['sh_ids_file'] = config.get('sortinghat', 'identities_file')
-        except configparser.NoOptionError:
-            logger.info("No identities files")
-
-
-        for backend in self.__get_backend_sections():
-            try:
-                raw = config.get(backend, 'raw_index')
-                enriched = config.get(backend, 'enriched_index')
-                conf[backend] = {'raw_index':raw, 'enriched_index':enriched}
-                for p in config[backend]:
-                    try:
-                        conf[backend][p] = config.getboolean(backend, p)
-                    except ValueError:
-                        conf[backend][p] = config.get(backend, p)
-            except configparser.NoSectionError:
-                pass
-
-        return conf
+        self.conf = Config(conf_file).get_conf()
 
     def check_es_access(self):
         ##
@@ -181,14 +89,6 @@ class Mordred:
                 raise ElasticSearchError(ES_ERROR % {'uri' : _ofuscate_server_uri(es)})
 
 
-    def __get_backend_sections(self):
-        # a backend name could include and extra ":<param>"
-        # to have several backend entries with different configs
-        gelk_backends = list(get_connectors().keys())
-        extra_backends = ["google_hits", "remo:activities"]
-
-        return gelk_backends + extra_backends
-
     def _get_repos_by_backend(self):
         #
         # return dict with backend and list of repositories
@@ -196,7 +96,7 @@ class Mordred:
         output = {}
         projects = self.conf['projects']
 
-        for backend_section in self.__get_backend_sections():
+        for backend_section in Config.get_backend_sections():
             for pro in projects:
                 backend = Task.get_backend(backend_section)
                 if backend in projects[pro]:
