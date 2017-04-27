4c66ee48663f5f993423c29f8f629042943400f6
Alvaro del Castillo <acs@bitergia.com>
2017-04-27 08:16:27 +0200

    [config] Add a set_param method to change a config param

--- mordred/config.py
+++ mordred/config.py
@@ -353,7 +353,15 @@ class Config():
             parser.write(f)
 
     def get_conf(self):
+        # TODO: Return a deepcopy to avoid uncontrolled changes in config?
         return self.conf
+
+    def set_param(self, section, param, value):
+        """ Change a param in the config """
+        if section not in self.conf or param not in self.conf[section]:
+            logger.error('Config section %s and param %s not exists', section, param)
+        else:
+            self.conf[section][param] = value
 
     @classmethod
     def get_backend_sections(cls):
