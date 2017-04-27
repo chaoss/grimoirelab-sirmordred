ec4406c1eb0a7a6649035e0a4facdc5ff7d0d277
Alvaro del Castillo <acs@bitergia.com>
2017-04-27 08:33:25 +0200

    [projects] Only use global config for bugzilla, gerrit and bugzillarest when the TaskProjects.GLOBAL_PROJECT is defined

--- mordred/task_projects.py
+++ mordred/task_projects.py
@@ -80,7 +80,8 @@ class TaskProjects(Task):
 
         for pro in projects:
             if backend in projects[pro]:
-                if backend in Config.get_global_data_sources() and pro != cls.GLOBAL_PROJECT:
+                if backend in Config.get_global_data_sources() \
+                    and cls.GLOBAL_PROJECT in projects and pro != cls.GLOBAL_PROJECT:
                     logger.debug("Skip global data source %s for project %s",
                                  backend, pro)
                 else:
