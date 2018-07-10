2bb2fb424c242b467c4f3a5dc09643ef859bb015
Valerio Cosentino <valcos@bitergia.com>
2018-07-10 17:15:00 +0200

    [tests] Add twitter archive
    This code enhances the task collection tests to include the
    perceval twitter backend.

--- tests/test_task_collection.py
+++ tests/test_task_collection.py
@@ -109,7 +109,7 @@ class TestTaskRawDataCollection(unittest.TestCase):
         backend_sections = ['askbot', 'bugzilla', 'bugzillarest', 'confluence',
                             'discourse', 'dockerhub', 'gerrit', 'github', 'jenkins',
                             'jira', 'mediawiki', 'meetup', 'mozillaclub', 'nntp', 'phabricator',
-                            'redmine', 'remo', 'rss', 'stackexchange', 'slack', 'telegram']
+                            'redmine', 'remo', 'rss', 'stackexchange', 'slack', 'telegram', 'twitter']
 
         for backend_section in backend_sections:
             task = TaskRawDataCollection(config, backend_section=backend_section)
