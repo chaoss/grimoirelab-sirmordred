132852df83ac70f03fbf39fda98b295425bb9e84
Valerio Cosentino <valcos@bitergia.com>
2018-08-24 17:51:22 +0200

    [tests] Add tests for googlehits
    This code includes test data for googlehits connectors.

--- tests/test_task_collection.py
+++ tests/test_task_collection.py
@@ -107,7 +107,7 @@ class TestTaskRawDataCollection(unittest.TestCase):
         config = Config(conf_file)
 
         backend_sections = ['askbot', 'bugzilla', 'bugzillarest', 'confluence',
-                            'discourse', 'dockerhub', 'gerrit', 'github', 'gitlab', 'jenkins',
+                            'discourse', 'dockerhub', 'gerrit', 'github', 'gitlab', 'google_hits', 'jenkins',
                             'jira', 'mediawiki', 'meetup', 'mozillaclub', 'nntp', 'phabricator',
                             'redmine', 'remo', 'rss', 'stackexchange', 'slack', 'telegram', 'twitter']
 
